334a083c5a2af7ba8d53b0ec8dfcfe0a94c5ffe6
Harbhajan Rai <kishanrai99999@gmail.com>
2019-03-25 14:52:35 +0530

    Add files via upload

---
+++ JavaEmail.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author harbhajan.rai
+ */
+import java.util.Properties;    
+import java.util.Scanner;
+import javax.mail.*;    
+import javax.mail.internet.*;    
+class Mailer{
+    public static void send(String from,String password,String to){
+        
+        //Get properties object
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        //get Session by authenticating emailID
+        Session session;
+        session = Session.getDefaultInstance(
+                props,new javax.mail.Authenticator(){
+                    protected PasswordAuthentication getPasswordAuthentication(){
+                        return new PasswordAuthentication(from,password);
+                    }
+                }
+        );
+        //compose message    
+        try
+        {
+            MimeMessage message = new MimeMessage(session);
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
+            
+            // set subject to mail
+            String sub = "enter you subject";
+            message.setSubject(sub);
+            
+            //set message in mail
+            String msg = "enter body of the mail";
+            message.setText(msg, "UTF-8");
+            
+            //send message
+            Transport.send(message);
+            System.out.println("Your Mail Sent");
+        }catch (MessagingException e)
+        {
+            throw new RuntimeException(e);
+        }    
+             
+    }  
+}  
+public class JavaEmail{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        //mailid which will be used to send mail
+        System.out.print("Email ID for sending mail : ");
+        String from = in.nextLine();
+        
+        //password of the mailid used to send mail
+        System.out.print("Password to authenticate the email ID : ");
+        String password = in.nextLine();
+        
+        //emailid to whom email has to be sent
+        System.out.print("Email to whom mail has to be sent : ");
+        String to = in.nextLine();
+        Mailer.send(from, password, to);
+    }   
+} 
